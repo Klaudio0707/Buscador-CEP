4340652f6829b584fc0874ae99823c3242b31a37
Cláudio Roberto <claudioroberto0707@outlook.com>
2024-10-21 00:23:35 -0300

    estilização feita

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-
-import './App.css';
-
+import { FiSearch } from "react-icons/fi";
+import './style.css';
 function App() {
   return (
-    <div className="App">
-   <h1>Hello word</h1>
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+      <div className='containerInput'>
+        <input type="text"
+          placeholder="Digite seu CEP... " />
+
+        <button className="buttonSearch">
+          <FiSearch size={25} color="#000" />
+        </button>
+      </div>
+      <main className="main">
+        <h2>CEP: 79</h2>
+        <span>Rua teste ALgum</span>
+        <span>Complemento: Algum complemento</span>
+        <span>Vila Rosa</span>
+        <span>Campo Grande</span>
+
+
+      </main>
     </div>
   );
 }
